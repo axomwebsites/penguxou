74f2701e1a79e6ed4a5bf4fb3eeb5038156e375b
axom websites <adamowaissi44@gmail.com>
2026-06-16 19:54:01 +0300

    Create healthsyrup.js

---
+++ potions/healthsyrup.js
@@ -0,0 +1,2 @@
+window.potions = window.potions || {};
+window.potions.healthsyrup = { id: "healthsyrup", name: "health syrup", cost: 20, type: "medicine", stat: "health", value: 25, icon: "fa-flask" };
