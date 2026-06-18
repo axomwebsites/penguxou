e97bb268ab43a2d26f42b8dc3608102077b40285
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:37:29 +0300

    Create items.js

---
+++ js/market/clothes/items.js
@@ -0,0 +1,4 @@
+export const clothesitems = [
+    { name: 'Cool Hat', type: 'clothes', cost: 50, value: 0, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634232420/1000067503.jpg' },
+    { name: 'Sporty Glasses', type: 'clothes', cost: 40, value: 0, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634257920/1000067506.jpg' }
+];
