8c0c90fa0172ae38635d1ba364ed21fb06fb572e
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:37:50 +0300

    Create items.js

---
+++ js/market/potions/items.js
@@ -0,0 +1,4 @@
+export const potionitems = [
+    { name: 'Health Potion', type: 'potion', cost: 30, value: 20, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634257920/1000067506.jpg' },
+    { name: 'Energy Potion', type: 'potion', cost: 30, value: 20, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634262701/1000067505.jpg' }
+];
