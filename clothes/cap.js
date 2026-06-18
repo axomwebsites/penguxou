62b77c790f69c250d5ece6b2c48fd03ba82de15a
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:20:54 +0300

    Delete clothes directory

--- clothes/cap.js
+++
@@ -1,2 +0,0 @@
-window.clothes = window.clothes || {};
-window.clothes.cap = { id: "cap", name: "baseball cap", cost: 25, type: "hat", icon: "fa-heading" };
--- clothes/royalhat.js
+++
@@ -1,2 +0,0 @@
-window.clothes = window.clothes || {};
-window.clothes.royalhat = { id: "royalhat", name: "royal crown", cost: 60, type: "hat", icon: "fa-graduation-cap" };
