e41caf953c74f6f2f34e69435c0d4c986ec6e85b
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:28:09 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import { initroomnavigation } from '#room';
+import { initpet } from '#pet';
+import { initshop } from '#shop';
+import { initinventory } from '#inventory';
+import { initgames } from '#games';
+
+initroomnavigation();
+initpet();
+initshop();
+initinventory();
+initgames();
