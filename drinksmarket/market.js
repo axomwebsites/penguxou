6b2037fae9ce2ad563d9e3da583757cc86bc0096
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:21:05 +0300

    Delete drinksmarket directory

--- drinksmarket/market.js
+++
@@ -1,5 +0,0 @@
-window.markets = window.markets || {};
-window.markets.drinks = [
-  { id: "water", name: "spring water", cost: 5, type: "food", value: 8, icon: "fa-tint" },
-  { id: "milk", name: "whole milk", cost: 12, type: "food", value: 18, icon: "fa-glass-martini-alt" }
-];
