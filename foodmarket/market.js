0113c83160f018e285c8a01a6b7cf78d94764351
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:21:16 +0300

    Delete foodmarket directory

--- foodmarket/market.js
+++
@@ -1,6 +0,0 @@
-window.markets = window.markets || {};
-window.markets.food = [
-  { id: "apple", name: "fresh apple", cost: 10, type: "food", value: 15, icon: "fa-apple-alt" },
-  { id: "fish", name: "raw fish", cost: 15, type: "food", value: 25, icon: "fa-fish" },
-  { id: "bread", name: "sliced bread", cost: 8, type: "food", value: 12, icon: "fa-bread-slice" }
-];
