3fe303faa88b49eaa78b626c135ebcaf07183ad8
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:36:56 +0300

    Create items.js

---
+++ js/market/drinksmarket/items.js
@@ -0,0 +1,5 @@
+export const drinkitems = [
+    { name: 'Bonjus Pack', type: 'drink', cost: 15, value: 18, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634306918/1000067517.jpg' },
+    { name: 'Milk Carton', type: 'drink', cost: 12, value: 15, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634302394/1000067513.jpg' },
+    { name: 'Mineral Water', type: 'drink', cost: 5, value: 8, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634296651/1000067512.jpg' }
+];
