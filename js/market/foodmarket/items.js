dda16c245294d1205a86ee6138a7f85079cc1a17
axom websites <adamowaissi44@gmail.com>
2026-06-18 20:36:22 +0300

    Create items.js

---
+++ js/market/foodmarket/items.js
@@ -0,0 +1,6 @@
+export const fooditems = [
+    { name: 'Burger', type: 'food', cost: 25, value: 30, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634251192/1000067504.jpg' },
+    { name: 'Bread Slice', type: 'food', cost: 10, value: 12, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634290172/1000067511.jpg' },
+    { name: 'Fresh Fish', type: 'food', cost: 20, value: 25, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634283035/1000067510.jpg' },
+    { name: 'Red Apple', type: 'food', cost: 8, value: 10, img: 'https://uploads.onecompiler.com/43xa2dbu5/1781634277484/1000067509.jpg' }
+];
